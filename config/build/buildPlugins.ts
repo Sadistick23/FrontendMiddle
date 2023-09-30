ba1a4da364f301158cfdc03eb4f23e5d8187ebc8
Sadistick23 <ariku.1@yandex.com>
2023-09-30 14:51:09 +0600

    Lesson 5

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,15 @@
 import webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins({paths, mode}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
         new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css'
+        }),
         new HTMLWebpackPlugin(
             {
                 template: paths.html,
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import React, {useState} from 'react';
-import './App.scss'
+import React from 'react';
+import Counter from "./Components/Counter/Counter";
+import './index.scss'
 
 const App = () => {
-    const [count, setCount] = useState(0)
-
-    const increments = () => {
-        setCount(count + 1)
-    }
-
     return (
-        <div>
-            {count}
-            <button onClick={increments}>
-                <div className="div">
-                    Increment
-                </div>
-            </button>
+        <div className='App'>
+            <Counter />
             Hello world!!!
         </div>
     );
