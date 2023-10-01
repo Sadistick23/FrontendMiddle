1dd267db8d072c26e68614bc9e86202bab8d4344
Sadistick23 <ariku.1@yandex.com>
2023-10-01 12:06:46 +0600

    Lesson 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Counter from "./Components/Counter/Counter";
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./Pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./Pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className='App'>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/'}>Main</Link>
             <Counter />
             Hello world!!!
+
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                    </Routes>
+                </Suspense>
+
         </div>
     );
 };
