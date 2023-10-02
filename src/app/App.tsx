64b60ff0ad6fc97b5112610d56539e5053df9f0f
Sadistick23 <ariku.1@yandex.com>
2023-10-02 14:54:59 +0600

    Lesson 9-10

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import React, {Suspense} from 'react';
+import './styles/index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "Pages/AboutPage";
+import {MainPage} from "Pages/MainPage";
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>Сменить тему</button>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/'}>Main</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -6,8 +6,9 @@ import {BuildEnv, BuildPaths} from "./config/build/types/config";
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
         build: path.resolve(__dirname, 'build'),
-        entry: path.resolve(__dirname, "src", "index.tsx"),
+        entry: path.resolve(__dirname, "src", "index"),
         html: path.resolve(__dirname, 'public', "index.html"),
+        src: path.resolve(__dirname, 'src')
     }
 
     const mode = env.mode || 'development'
