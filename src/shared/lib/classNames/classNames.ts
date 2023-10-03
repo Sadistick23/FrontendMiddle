c6e58480c7d6fc69df41840d118596a292f63736
Sadistick23 <ariku.1@yandex.com>
2023-10-03 12:22:02 +0600

    Lesson 12

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -6,10 +6,10 @@ type Mods = Record<string, boolean | string>
     'disabled': false,
 }*/
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods)
             .filter(([cls, value]) => Boolean(value))
             .map(([cls, value]) => cls)
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from "./Navbar.module.scss"
+import {classNames} from "shared/lib/classNames/classNames";
+import AppLink, {AppLickTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string;
+}
+
+const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(styles.nav, {}, [className])}>
+            <div className={styles.links}>
+                <AppLink theme={AppLickTheme.SECONDARY} to={'/about'} className={styles.mainLink}>About</AppLink>
+                <AppLink theme={AppLickTheme.SECONDARY}  to={'/'}>Main</AppLink>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
